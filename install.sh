83e26c33dd997645d21415043d57ec7e529efbb0
EEPROM <eepromchip@gmail.com>
2020-04-15 10:48:04 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,96 +1,164 @@
-#!/bin/bash
-set -o pipefail
+#!/usr/bin/env bash
+
+# Setup script environment
+set -o errexit  #Exit immediately if a pipeline returns a non-zero status
+set -o errtrace #Trap ERR from shell functions, command substitutions, and commands from subshell
+set -o nounset  #Treat unset variables as an error
+set -o pipefail #Pipe will exit with last non-zero status if applicable
 shopt -s expand_aliases
 alias die='EXIT=$? LINE=$LINENO error_exit'
 trap die ERR
+trap cleanup EXIT
+
 function error_exit() {
-    REASON=$1
-    MSG="\e[91mERROR: \e[93m$EXIT@"
-    if [ -z "$REASON" ]; then
-        MSG="$MSG$LINE:"
-        REASON="Unknown failure occured."
-    else
-        MSG="$MSG`echo $(( $LINE - 1 ))`:"
+  trap - ERR
+  local DEFAULT='Unknown failure occured.'
+  local REASON="\e[97m${1:-$DEFAULT}\e[39m"
+  local FLAG="\e[91m[ERROR] \e[93m$EXIT@$LINE"
+  msg "$FLAG $REASON"
+  [ ! -z ${VMID-} ] && cleanup_vmid
+  exit $EXIT
+}
+function warn() {
+  local REASON="\e[97m$1\e[39m"
+  local FLAG="\e[93m[WARNING]\e[39m"
+  msg "$FLAG $REASON"
+}
+function info() {
+  local REASON="$1"
+  local FLAG="\e[36m[INFO]\e[39m"
+  msg "$FLAG $REASON"
+}
+function msg() {
+  local TEXT="$1"
+  echo -e "$TEXT"
+}
+function cleanup_vmid() {
+  if $(qm status $VMID &>/dev/null); then
+    if [ "$(qm status $VMID | awk '{print $2}')" == "running" ]; then
+      qm stop $VMID
     fi
-    echo -e "$MSG \e[97m$REASON\e[39m\e[49m"
-    exit $EXIT
+    qm destroy $VMID
+  fi
 }
 function cleanup() {
-    popd >/dev/null
-    rm -rf $TMP
+  popd >/dev/null
+  rm -rf $TEMP_DIR
 }
-trap cleanup EXIT
-TMP=`mktemp -d`
-pushd $TMP >/dev/null
+TEMP_DIR=$(mktemp -d)
+pushd $TEMP_DIR >/dev/null
 
-STORAGE=${1:-local-lvm}
-pvesm list $STORAGE >& /dev/null ||
-    die "'$STORAGE' is not a valid storage ID."
-pvesm status -content images -storage $STORAGE >&/dev/null ||
-    die "'$STORAGE' does not allow 'Disk image' to be stored."
-STORAGE_TYPE=`pvesm status -storage $STORAGE | awk 'NR>1 {print $2}'`
-VMID=$(cat<<EOF | python3
-import json
-with open('/etc/pve/.vmlist') as vmlist:
-    vmids = json.load(vmlist)
-if 'ids' not in vmids:
-    print(100)
-else:
-    last_vm = sorted(vmids['ids'].keys())[+1:][0]
-    print(int(last_vm)+1)
-EOF
-)
-echo -e "\n\n\n" \
-    "********************************\n" \
-    "*  Getting latest HassOS Info  *\n" \
-    "********************************\n"
+# Select storage location
+while read -r line; do
+  TAG=$(echo $line | awk '{print $1}')
+  TYPE=$(echo $line | awk '{printf "%-10s", $2}')
+  FREE=$(echo $line | numfmt --field 4-6 --from-unit=K --to=iec --format %.2f | awk '{printf( "%9sB", $6)}')
+  ITEM="  Type: $TYPE Free: $FREE "
+  OFFSET=2
+  if [[ $((${#ITEM} + $OFFSET)) -gt ${MSG_MAX_LENGTH:-} ]]; then
+    MSG_MAX_LENGTH=$((${#ITEM} + $OFFSET))
+  fi
+  STORAGE_MENU+=( "$TAG" "$ITEM" "OFF" )
+done < <(pvesm status -content images | awk 'NR>1')
+if [ $((${#STORAGE_MENU[@]}/3)) -eq 0 ]; then
+  warn "'Disk image' needs to be selected for at least one storage location."
+  die "Unable to detect valid storage location."
+elif [ $((${#STORAGE_MENU[@]}/3)) -eq 1 ]; then
+  STORAGE=${STORAGE_MENU[0]}
+else
+  while [ -z "${STORAGE:+x}" ]; do
+    STORAGE=$(whiptail --title "Storage Pools" --radiolist \
+    "Which storage pool you would like to use for the container?\n\n" \
+    16 $(($MSG_MAX_LENGTH + 23)) 6 \
+    "${STORAGE_MENU[@]}" 3>&1 1>&2 2>&3) || exit
+  done
+fi
+info "Using '$STORAGE' for storage location."
+
+# Get the next guest VM/LXC ID
+VMID=$(pvesh get /cluster/nextid)
+info "Container ID is $VMID."
+
+# Get latest Home Assistant disk image archive URL
+msg "Getting URL for latest Home Assistant disk image..."
+RELEASE_EXT=vmdk.gz
 URL=$(cat<<EOF | python3
 import requests
 url = 'https://api.github.com/repos/home-assistant/hassos/releases/latest'
 r = requests.get(url).json()
 if 'message' in r:
-    exit()
+  exit()
 for asset in r['assets']:
-    if asset['name'].endswith('vdi.gz'):
-        print(asset['browser_download_url'])
+  if asset['name'].endswith('$RELEASE_EXT'):
+    print(asset['browser_download_url'])
 EOF
 )
 if [ -z "$URL" ]; then
-    die "Github has returned an error. A rate limit may have been applied to your connection."
+  die "Github has returned an error. A rate limit may have been applied to your connection."
 fi
-echo -e "\n\n\n" \
-    "********************************\n" \
-    "*      Downloading HassOS      *\n" \
-    "********************************\n"
+
+# Download Home Assistant disk image archive
+msg "Downloading disk image..."
 wget -q --show-progress $URL
+echo -en "\e[1A\e[0K" #Overwrite output from wget
 FILE=$(basename $URL)
-echo -e "\n\n\n" \
-    "********************************\n" \
-    "*      Extracting HassOS       *\n" \
-    "********************************\n"
+
+# Extract Home Assistant disk image
+msg "Extracting disk image..."
 gunzip -f $FILE
-echo -e "\n\n\n" \
-    "********************************\n" \
-    "*       Creating new VM        *\n" \
-    "********************************\n"
+
+# Create variables for container disk
+STORAGE_TYPE=$(pvesm status -storage $STORAGE | awk 'NR>1 {print $2}')
 if [ "$STORAGE_TYPE" = "dir" ]; then
-    DISK_EXT=".qcow2"
-    DISK_REF="$VMID/"
-    IMPORT_OPT="-format qcow2"
+  DISK_EXT=".qcow2"
+  DISK_REF="$VMID/"
+  IMPORT_OPT="-format qcow2"
 fi
 for i in {0,1}; do
-    disk="DISK$i"
-    eval DISK${i}=vm-${VMID}-disk-${i}${DISK_EXT}
-    eval DISK${i}_REF=${STORAGE}:$DISK_REF${!disk}
+  disk="DISK$i"
+  eval DISK${i}=vm-${VMID}-disk-${i}${DISK_EXT:-}
+  eval DISK${i}_REF=${STORAGE}:${DISK_REF:-}${!disk}
 done
-qm create $VMID -bios ovmf -name $(sed -e "s/\_//g" -e "s/.vdi.gz//" <<< $FILE) \
-    -net0 virtio,bridge=vmbr0 -onboot 1 -ostype l26 -scsihw virtio-scsi-pci
+
+# Create VM
+msg "Creating VM..."
+VM_NAME=$(sed -e "s/\_//g" -e "s/.${RELEASE_EXT}//" <<< $FILE)
+qm create $VMID -agent 1 -bios ovmf -name $VM_NAME -net0 virtio,bridge=vmbr0 \
+  -onboot 1 -ostype l26 -scsihw virtio-scsi-pci
 pvesm alloc $STORAGE $VMID $DISK0 128 1>&/dev/null
-qm importdisk $VMID ${FILE%".gz"} $STORAGE $IMPORT_OPT 1>&/dev/null
+qm importdisk $VMID ${FILE%".gz"} $STORAGE ${IMPORT_OPT:-} 1>&/dev/null
 qm set $VMID -bootdisk sata0 -efidisk0 ${DISK0_REF},size=128K \
-    -sata0 ${DISK1_REF},size=6G > /dev/null
-echo -e "\n\n\n" \
-    "********************************\n" \
-    "*    Completed Successfully    *\n" \
-    "*       New VM ID is \e[1m$VMID\e[0m       *\n" \
-    "********************************\n"
+  -sata0 ${DISK1_REF},size=6G > /dev/null
+
+# Add serial port and enable console output
+set +o errtrace
+(
+  msg "Adding serial port and configuring console..."
+  trap '
+    warn "Unable to configure serial port. VM is still functional."
+    if [ "$(qm config $VMID | sed -n ''/serial0/p'')" != "" ]; then
+      qm set $VMID --delete serial0 >/dev/null
+    fi
+    exit
+  ' ERR
+  if [ "$(command -v kpartx)" = "" ]; then
+    msg "Installing 'kpartx'..."
+    apt-get update >/dev/null
+    apt-get -qqy install kpartx &>/dev/null
+  fi
+  DISK1_PATH="$(pvesm path $DISK1_REF)"
+  DISK1_PART1="$(kpartx -al $DISK1_PATH | awk 'NR==1 {print $1}')"
+  DISK1_PART1_PATH="/dev/mapper/$DISK1_PART1"
+  TEMP_MOUNT="${TEMP_DIR}/mnt"
+  trap '
+    findmnt $TEMP_MOUNT >/dev/null && umount $TEMP_MOUNT
+    command -v kpartx >/dev/null && kpartx -d $DISK1_PATH
+  ' EXIT
+  kpartx -a $DISK1_PATH
+  mkdir $TEMP_MOUNT
+  mount $DISK1_PART1_PATH $TEMP_MOUNT
+  sed -i 's/$/ console=ttyS0/' ${TEMP_MOUNT}/cmdline.txt
+  qm set $VMID -serial0 socket >/dev/null
+)
+
+info "Completed Successfully! New VM ID is \e[1m$VMID\e[0m."
